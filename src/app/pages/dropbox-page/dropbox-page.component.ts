5d55de443a72672cb330f5a8b63fd645cfb41226
Darren <darren@darrenmothersele.com>
2019-01-04 16:09:05 +0000

    add download/delete dropbox options

--- src/app/pages/dropbox-page/dropbox-page.component.ts
+++ src/app/pages/dropbox-page/dropbox-page.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { HttpClient } from '@angular/common/http';
-import { head } from 'lodash-es';
 
 @Component({
   selector: 'app-dropbox-page',
@@ -13,12 +12,16 @@ export class DropboxPageComponent implements OnInit {
   files$;
   accessToken;
 
+  showSync = false;
+
   constructor(
     private readonly auth: AuthService,
-    private readonly http: HttpClient
+    private readonly http: HttpClient,
+    private readonly zone: NgZone
   ) { }
 
   ngOnInit() {
+    this.showSync = false;
     const idp = this.auth.getIdentity('dropbox');
     if (!!idp && idp.access_token) {
       const { access_token } = idp;
@@ -30,6 +33,33 @@ export class DropboxPageComponent implements OnInit {
       };
       this.files$ = this.http.post(url, body, { headers });
     }
+    setTimeout(() => {
+      this.zone.run(() => {
+        this.showSync = true;
+      });
+    }, 1000);
+  }
+
+  onDownload(f) {
+    const path = f.id;
+    const headers = {
+      Authorization: `Bearer ${this.accessToken}`
+    };
+    const url = 'https://api.dropboxapi.com/2/files/get_temporary_link';
+    this.http.post<any>(url, { path }, { headers }).subscribe(({ link }) => {
+      window.open(link);
+    });
+  }
+
+  onDelete(f) {
+    const url = 'https://api.dropboxapi.com/2/files/delete_v2';
+    const path = f.id;
+    const headers = {
+      Authorization: `Bearer ${this.accessToken}`
+    };
+    this.http.post<any>(url, { path }, { headers }).subscribe(() => {
+      this.ngOnInit();
+    });
   }
 
   onFilePick(event) {
@@ -42,7 +72,9 @@ export class DropboxPageComponent implements OnInit {
         'Content-Type': 'application/octet-stream'
       };
       const url = 'https://content.dropboxapi.com/2/files/upload';
-      this.http.post(url, file, { headers }).subscribe(console.log);
+      this.http.post(url, file, { headers }).subscribe(() => {
+        this.ngOnInit();
+      });
     }
   }
 
@@ -53,20 +85,3 @@ export class DropboxPageComponent implements OnInit {
   }
 
 }
-
-/*
-curl -X POST  \
-    --header "Authorization: Bearer
-    --header "Content-Type: application/json" \
-    --data "{\"path\": \"/Homework/math\",\"recursive\": false,\"include_media_info\":
-    false,\"include_deleted\": false,\"include_has_explicit_shared_members\":
-     false,\"include_mounted_folders\": true}"
-
-
-     curl -X POST https://content.dropboxapi.com/2/files/upload \
-    --header "Authorization: Bearer
-    --header "Dropbox-API-Arg: {\"path\": \"/Homework/math/Matrices.txt\",
-    \"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" \
-    --header "Content-Type: application/octet-stream" \
-    --data-binary @local_file.txt
- */
