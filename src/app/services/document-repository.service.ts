65f4177fd95dd813db5b7b188786bcfa6bc3c109
Darren <darren@darrenmothersele.com>
2019-01-07 12:41:43 +0000

    fix linter errors

--- src/app/services/document-repository.service.ts
+++ src/app/services/document-repository.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from "rxjs";
+import { from, Observable } from 'rxjs';
 import PouchDB from 'pouchdb';
 import Find from 'pouchdb-find';
 import { v4 as UUIDv4 } from 'uuid';
-import { SchemaRepositoryService } from "./schema-repository.service";
-import { switchMap } from "rxjs/operators";
+import { SchemaRepositoryService } from './schema-repository.service';
+import { switchMap } from 'rxjs/operators';
 import { at, omit } from 'lodash-es';
 
 declare const emit;
@@ -24,14 +24,14 @@ export class DocumentRepositoryService {
   init() {
     PouchDB.plugin(Find);
     this.db = new PouchDB('kendraio');
-    return this.db.createIndex({ index: { fields: [ "@label" ]}})
+    return this.db.createIndex({ index: { fields: [ '@label' ]}})
       .then(() => this.db.get('_design/kendraio_docs').catch(() => ({ _rev: null })))
       .then(({ _rev }) => !_rev ? this.db.put({
         _id: '_design/kendraio_docs',
         _rev,
         views: {
           by_label: {
-            map: function (doc) { emit(doc["@label"]); }.toString()
+            map: function (doc) { emit(doc['@label']); }.toString()
           }
         } as any
       }) : {});
@@ -43,12 +43,12 @@ export class DocumentRepositoryService {
 
   addNew(schemaName: string): Observable<PouchDB.Core.Response> {
     const _id = `${schemaName}:${UUIDv4()}`;
-    return this.putDoc({ _id, "@schema": schemaName });
+    return this.putDoc({ _id, '@schema': schemaName });
   }
 
   getDoc(id) {
     return from(this.db.get(id, { attachments: true, binary: true }).then(doc => {
-      const { attachments } = this.schemaRepo.getSchema(doc["@schema"]);
+      const { attachments } = this.schemaRepo.getSchema(doc['@schema']);
       return (attachments || []).reduce((a, v) => {
         if (doc['_attachments'] && doc['_attachments'][v]) {
           a[v] = doc['_attachments'][v]['data'];
@@ -61,10 +61,10 @@ export class DocumentRepositoryService {
   }
 
   putDoc(doc) {
-    const { labelField, attachments } = this.schemaRepo.getSchema(doc["@schema"]);
+    const { labelField, attachments } = this.schemaRepo.getSchema(doc['@schema']);
     return from(this.db.put({
       ...omit(doc, attachments),
-      "@label": doc[labelField]
+      '@label': doc[labelField]
     }).then((r) => {
       return Promise.all((attachments || []).map(key => {
         if (doc && doc[key]) {
