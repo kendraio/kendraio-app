c83e67ea8c9f654b952bd210acfc710258fdedd0
Darren <darren@darrenmothersele.com>
2019-01-29 13:06:17 +0000

    add confirmation to the database delete

--- src/app/pages/docs-list-page/docs-list-page.component.ts
+++ src/app/pages/docs-list-page/docs-list-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DocumentRepositoryService } from "../../services/document-repository.service";
 import { PageTitleService } from '../../services/page-title.service';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { ConfirmDeleteDialogComponent } from '../../dialogs/confirm-delete-dialog/confirm-delete-dialog.component';
 
 @Component({
   selector: 'app-docs-list-page',
@@ -15,7 +17,8 @@ export class DocsListPageComponent implements OnInit {
   constructor(
     private readonly pageTitle: PageTitleService,
     private readonly router: Router,
-    private readonly docsRepo: DocumentRepositoryService
+    private readonly docsRepo: DocumentRepositoryService,
+    private readonly dialog: MatDialog
   ) { }
 
   ngOnInit() {
@@ -23,10 +26,22 @@ export class DocsListPageComponent implements OnInit {
     this.docs$ = this.docsRepo.listAll();
   }
 
-  deleteDoc(id) {
-    this.docsRepo.deleteDoc(id).subscribe(({ ok }) => {
-      console.log(`Deleted: ${ok}`);
-      this.docs$ = this.docsRepo.listAll();
+  deleteDoc(doc) {
+    const dialogRef = this.dialog.open(ConfirmDeleteDialogComponent, {
+      data: {
+        item: {
+          type: doc.id.split(':')[0],
+          name: doc.key
+        }
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.docsRepo.deleteDoc(doc.id).subscribe(({ok}) => {
+          console.log(`Deleted: ${ ok }`);
+          this.docs$ = this.docsRepo.listAll();
+        });
+      }
     });
   }
 
