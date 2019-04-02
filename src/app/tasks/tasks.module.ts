3dedb94861e91dd4da7fd720aa47eca89b879458
tim-deighton <tim.warp@gmail.com>
2019-04-02 14:44:11 +0100

    TASKS: added overview cards

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TasksRoutingModule } from './tasks-routing.module';
-import { TasksComponent } from './tasks/tasks.component';
+import { TasksComponent, DialogDataDialogComponent } from './tasks/tasks.component';
 import { SharedModule } from '../_shared/shared.module';
 import { AppMaterialModule } from '../app-material/app-material.module';
 
 
 @NgModule({
-  declarations: [TasksComponent],
+  declarations: [TasksComponent, DialogDataDialogComponent],
+  entryComponents: [DialogDataDialogComponent],
   imports: [
     CommonModule,
     TasksRoutingModule,
