1130877c4d8cd9b534c7362bf513c7ec9b264153
Darren <darren@darrenmothersele.com>
2019-08-15 10:10:18 +0100

    add json schema to form builder

--- src/app/pages/form-builder-page/form-builder-page.component.ts
+++ src/app/pages/form-builder-page/form-builder-page.component.ts
@@ -6,7 +6,10 @@ import {FormSubmitHandlerService} from '../../services/form-submit-handler.servi
 import {ShareLinkGeneratorService} from '../../services/share-link-generator.service';
 import {Subject} from 'rxjs';
 import {debounceTime, takeUntil} from 'rxjs/operators';
-import JSONFormatter from 'json-formatter-js'
+import JSONFormatter from 'json-formatter-js';
+import { JSON_SCHEMA } from './jsonschema';
+import { NgxEditorModel } from 'ngx-monaco-editor';
+import {config} from '../../_shared/ui-form/config';
 
 @Component({
   selector: 'app-form-builder-page',
@@ -16,16 +19,30 @@ import JSONFormatter from 'json-formatter-js'
 export class FormBuilderPageComponent implements OnInit, OnDestroy {
 
   editorOptions = {
+    baseUrl: 'form-builder',
     theme: 'vs-dark',
     language: 'json',
     minimap: {
       enabled: false
     },
-    lineNumbers: 'off'
+    lineNumbers: 'off',
+    scrollBeyondLastLine: false
   };
 
   JSONSchema = '{}';
+  jsonModel: NgxEditorModel = {
+    value: this.JSONSchema,
+    language: 'json',
+    uri: 'a:jsonModel.json'
+  };
+
   UISchema = '{}';
+  uiModel: NgxEditorModel = {
+    value: this.UISchema,
+    language: 'json',
+    uri: 'a:uiModel.json'
+  };
+
   modelText = '{}';
 
   form = new FormGroup({});
@@ -69,6 +86,59 @@ export class FormBuilderPageComponent implements OnInit, OnDestroy {
     this._destroy$.complete();
   }
 
+  initJson() {
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      schemas: [{
+        fileMatch: ['a:jsonModel.json'],
+        uri: 'https://json-schema.org/draft-07/schema',
+        schema: JSON_SCHEMA
+      }, {
+        fileMatch: ['a:uiModel.json'],
+        uri: 'https://app.kendra.io/v0/ui-schema',
+        schema: {
+          type: 'object',
+          additionalProperties: {
+            type: 'object',
+            properties: {
+              'ui:widget': {
+                'type': 'string',
+                'enum': [
+                  'datepicker',
+                  ...config.types.map(({ name }) => name)
+                ]
+              },
+              'ui:disabled': {
+                'type': 'boolean'
+              },
+              'ui:placeholder': {
+                'type': 'boolean'
+              },
+              'ui:labelProp': {
+                type: 'string'
+              },
+              'ui:valueProp': {
+                type: 'string'
+              },
+              'ui:isMultiSelect': {
+                type: 'boolean'
+              },
+              'ui:ref': {
+                type: 'string'
+              },
+              'ui:refType': {
+                enum: ['json', 'http', 'fetch']
+              }
+            }
+          }
+        }
+      }]
+    });
+  }
+
+  initUi() {
+  }
+
   jsonSchemaChange() {
     this._schemaChange$.next();
   }
@@ -81,7 +151,7 @@ export class FormBuilderPageComponent implements OnInit, OnDestroy {
 
   onChange() {
     // Replace #modelOutput DIV contents with formatted JSON
-    const formatter = new JSONFormatter(this.model, 1, { theme: 'dark' });
+    const formatter = new JSONFormatter(this.model, Infinity, { theme: 'dark' });
     while (this.modelOutput.nativeElement.firstChild) {
       this.modelOutput.nativeElement.removeChild(this.modelOutput.nativeElement.firstChild);
     }
