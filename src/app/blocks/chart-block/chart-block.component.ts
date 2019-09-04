f45ab0c606b773e3b98c18bdb4a7275cca86b3c5
Darren <darren@darrenmothersele.com>
2019-09-04 09:43:35 +0100

    enable chart block

--- src/app/blocks/chart-block/chart-block.component.ts
+++ src/app/blocks/chart-block/chart-block.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, ViewChild} from '@angular/core';
+import {get, isArray, isObject} from 'lodash-es';
 
 @Component({
   selector: 'app-chart-block',
   templateUrl: './chart-block.component.html',
   styleUrls: ['./chart-block.component.scss']
 })
-export class ChartBlockComponent implements OnInit {
+export class ChartBlockComponent implements OnInit, OnChanges {
 
   rowData = [];
   outputConfig = {};
+
+  @Input() config;
+  @Input() model: any = {};
+  @Output() output = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes) {
+    this.updateOutputDisplay();
+    this.output.emit(isArray(this.model) ? [ ...this.model ] : isObject(this.model) ? { ...this.model } : this.model);
+  }
+
+  updateOutputDisplay() {
+    this.rowData = isArray(this.model) ? this.model : get(this.model, 'result', []);
+  }
 }
