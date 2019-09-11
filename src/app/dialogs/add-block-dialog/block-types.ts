fabd50d80f20e84de1da1da0418bc8c770b8c688
Darren <darren@darrenmothersele.com>
2019-09-11 12:14:13 +0100

    add blocks editor

--- src/app/dialogs/add-block-dialog/block-types.ts
+++ src/app/dialogs/add-block-dialog/block-types.ts
@@ -4,6 +4,7 @@ export const BLOCK_TYPES = [
     label: 'Actions',
     description: 'Display a row of action buttons with related workflows.',
     icon: 'edit_attributes',
+    hasEditor: true,
     defaultConfig: {
       type: 'actions',
       buttons: [
@@ -19,6 +20,7 @@ export const BLOCK_TYPES = [
     label: 'Batch process',
     description: 'Run a selected workflow against every item in a list.',
     icon: 'fa-list',
+    hasEditor: true,
     defaultConfig: {
       type: 'batch',
       blocks: []
@@ -47,6 +49,7 @@ export const BLOCK_TYPES = [
     label: 'Dialog',
     description: 'Launch a modal dialog in which to run an embedded workflow.',
     icon: 'fa-running',
+    hasEditor: true,
     defaultConfig: {
       type: 'dialog',
       blocks: []
@@ -57,6 +60,7 @@ export const BLOCK_TYPES = [
     label: 'Event dispatch',
     description: 'Dispatch an event to trigger asynchronous processing',
     icon: 'fa-rocket',
+    hasEditor: true,
     defaultConfig: {
       type: 'dispatch',
       action: '[TASK_NAME]'
@@ -67,6 +71,7 @@ export const BLOCK_TYPES = [
     label: 'Form',
     description: 'Display a form for data entry or editing.',
     icon: 'fa-keyboard',
+    hasEditor: true,
     defaultConfig: {
       type: 'form',
       jsonSchema: {},
@@ -78,6 +83,7 @@ export const BLOCK_TYPES = [
     label: 'Data grid',
     description: 'Display a data grid of the current modal data values',
     icon: 'fa-table',
+    hasEditor: true,
     defaultConfig: {
       type: 'grid',
       gridOptions: {},
@@ -89,6 +95,7 @@ export const BLOCK_TYPES = [
     label: 'HTTP Request',
     description: 'Get, put or post data to an external HTTP endpoint.',
     icon: 'cloud',
+    hasEditor: true,
     defaultConfig: {
       type: 'http',
       method: 'get',
@@ -114,6 +121,7 @@ export const BLOCK_TYPES = [
     label: 'Mapping',
     description: 'Map data using JMESPath',
     icon: 'sync',
+    hasEditor: true,
     defaultConfig: {
       type: 'mapping',
       mapping: 'data'
@@ -124,6 +132,7 @@ export const BLOCK_TYPES = [
     label: 'Message',
     description: 'Display a customisable message based on the current data modal values.',
     icon: 'message',
+    hasEditor: true,
     defaultConfig: {
       type: 'message',
       message: ''
@@ -134,6 +143,7 @@ export const BLOCK_TYPES = [
     label: 'Multiplex',
     description: 'Display and process multiple workflows concurrently.',
     icon: 'fa-stream',
+    hasEditor: true,
     defaultConfig: {
       type: 'multi',
       batches: []
@@ -144,6 +154,7 @@ export const BLOCK_TYPES = [
     label: 'Query',
     description: 'Run a configurable query against a data source.',
     icon: 'fa-database',
+    hasEditor: true,
     defaultConfig: {
       type: 'query'
     }
@@ -153,6 +164,7 @@ export const BLOCK_TYPES = [
     label: 'Switch',
     description: 'Conditionally run workflows based on evaluation of input value.',
     icon: 'fa-random',
+    hasEditor: true,
     defaultConfig: {
       type: 'switch',
       eval: 'data',
@@ -164,6 +176,7 @@ export const BLOCK_TYPES = [
     label: 'Template',
     description: 'Display templated output based on current modal values.',
     icon: 'fa-file-code',
+    hasEditor: true,
     defaultConfig: {
       type: 'template',
       template: ''
@@ -174,6 +187,7 @@ export const BLOCK_TYPES = [
     label: 'Get variable',
     description: 'Fetch a value from local storage.',
     icon: 'file_upload',
+    hasEditor: true,
     defaultConfig: {
       type: 'variable-get',
       name: '[VARIABLE_NAME]'
@@ -184,6 +198,7 @@ export const BLOCK_TYPES = [
     label: 'Set variable',
     description: 'Save a value to local storage.',
     icon: 'file_download',
+    hasEditor: true,
     defaultConfig: {
       type: 'variable-set',
       name: '[VARIABLE_NAME]'
