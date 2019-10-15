1164048905ac75bc61871f82a3477d09e9348214
Darren <darren@darrenmothersele.com>
2019-10-15 10:17:36 +0100

    adding global error handler

---
+++ src/app/services/global-error-handler.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, ErrorHandler } from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+import {Subject} from 'rxjs';
+import {debounceTime, throttleTime} from 'rxjs/operators';
+
+@Injectable()
+export class GlobalErrorHandlerService implements ErrorHandler {
+
+  error$ = new Subject();
+
+  constructor(
+    private readonly notify: MatSnackBar
+  ) {
+    this.error$.pipe(
+      throttleTime(4000)
+    ).subscribe((error: any) => {
+      this.notify.dismiss();
+      if (error.message && error.message.length > 0) {
+        this.notify.open(error.message, 'Dismiss', { horizontalPosition: 'center', verticalPosition: 'top', duration: 4000 });
+      }
+    });
+  }
+
+  handleError(error: any): void {
+    console.log(error);
+    this.error$.next(error);
+  }
+}
