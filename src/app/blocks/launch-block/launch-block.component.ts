4c5fcb3e2414b3d6e67efa91724f86bcd113ea5c
Darren <darren@darrenmothersele.com>
2019-11-13 09:39:33 +0000

    add valueGetters support to launch block

--- src/app/blocks/launch-block/launch-block.component.ts
+++ src/app/blocks/launch-block/launch-block.component.ts
@@ -3,6 +3,7 @@ import {BaseBlockComponent} from '../base-block/base-block.component';
 import {Router} from '@angular/router';
 import {get, isArray, isObject, isString} from 'lodash-es';
 import {search} from 'jmespath';
+import {mappingUtility} from '../mapping-block/mapping-util';
 
 @Component({
   selector: 'app-launch-block',
@@ -20,15 +21,23 @@ export class LaunchBlockComponent extends BaseBlockComponent {
   onData(data: any, firstChange: boolean) {
     if (!firstChange) {
       const context = this.getMappingResult(get(this.config, 'context', {}));
-      const adapter = get(this.config, 'adapter');
-      const workflowId = get(this.config, 'workflowId');
+      const adapter = this.valueGetter('adapter');
+      const workflowId = this.valueGetter('workflowId');
       this.router.navigate([adapter, workflowId], { queryParams: context });
     }
   }
 
+  valueGetter(key) {
+    const expr = get(this.config, `valueGetters.${key}`);
+    if (!expr) {
+      return get(this.config, key);
+    }
+    return mappingUtility({ data: this.model, context: this.context }, expr);
+  }
+
   getMappingResult(mapping) {
     if (isString(mapping)) {
-      return search({ data: this.model, context: this.context }, mapping);
+      return mappingUtility({ data: this.model, context: this.context }, mapping);
     }
     if (isArray(mapping)) {
       return mapping.map(v => this.getMappingResult(v));
