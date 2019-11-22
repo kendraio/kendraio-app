86fe5ce4cffbdc6f3d37c4cdadff43dc06349968
tim-deighton <tim.warp@gmail.com>
2019-11-22 00:09:25 +0000

    updates to thumbnail-viewer

--- src/app/_shared/ui-form/types/thumbnail-viewer.component.ts
+++ src/app/_shared/ui-form/types/thumbnail-viewer.component.ts
@@ -21,6 +21,7 @@ role="option" *ngFor="let thumb of thumbs; index as i" (click)="selectThumb(i)"
 [ngClass]="{'default-thumb': thumb.isDefault}"
 src="{{thumb.url}}">
 
+{{formControl.value}}xxxxxxxxx {{videoUrl}}
 
 </div>
 
@@ -32,14 +33,33 @@ src="{{thumb.url}}">
 
 export class ThumbnailViewerComponent extends FieldType implements OnInit {
   @Input()
-  formControl;
-  videoId: any;
+  videoId: any = 'I_txFxMKg4U';
   videoUrl: any;
-  thumbs: Array<any>;
+  thumbs: Array<any> = [];
 
   ngOnInit() {
-    this.thumbs = this.formControl.value;
+
+    this.videoUrl = this.cleanUrl(this.formControl.value); // TODO: include whole uRL
+
+
+    // TODO: how do we locate the default thumb?
+    this.thumbs = [
+      {url: 'https://i.ytimg.com/vi/' + this.videoUrl + '/0.jpg', isDefault: true},
+{url: 'https://i.ytimg.com/vi/' + this.videoUrl + '/1.jpg', isDefault: false},
+{url: 'https://i.ytimg.com/vi/' + this.videoUrl + '/2.jpg', isDefault: true},
+{url: 'https://i.ytimg.com/vi/' + this.videoUrl + '/3.jpg', isDefault: false},
+    ];
+
+
+
+
+
   }
+
+  cleanUrl(urlStr: string) {
+    return urlStr.split('/')[4];
+    }
+
 
   selectThumb(i: number) {
     this.thumbs.forEach(t => {
