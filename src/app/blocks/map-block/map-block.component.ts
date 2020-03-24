d2bf87de736cdf5bb562317c8690df80ee129fce
Darren <darren@darrenmothersele.com>
2020-03-24 10:09:37 +0000

    fix marker images

--- src/app/blocks/map-block/map-block.component.ts
+++ src/app/blocks/map-block/map-block.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {BaseBlockComponent} from '../base-block/base-block.component';
-import {latLng, marker, tileLayer} from 'leaflet';
+import {icon, latLng, marker, tileLayer} from 'leaflet';
 import {get, isArray, set} from 'lodash-es';
 
 @Component({
@@ -37,7 +37,14 @@ contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA
 
   onData(data: any, firstChange: boolean) {
     if (isArray(data)) {
-      this.layers = data.map(({ lat, long, label }) => marker(latLng(lat, long)).bindPopup(label));
+      this.layers = data.map(({ lat, long, label }) => marker(latLng(lat, long), {
+        icon: icon({
+          iconSize: [ 25, 41 ],
+          iconAnchor: [ 13, 41 ],
+          iconUrl: '/assets/marker-icon.png',
+          shadowUrl: '/assets/marker-shadow.png'
+        })
+      }).bindPopup(label));
     }
   }
 }
