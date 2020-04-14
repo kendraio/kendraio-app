9f634bd02d3a87720ef31fcd3f238639757e6249
Darren <darren@darrenmothersele.com>
2020-04-14 15:37:12 +0100

    upgrade ag-grid

--- src/app/_shared/components/material/mat-button.component.ts
+++ src/app/_shared/components/material/mat-button.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {IHeaderAngularComp, ICellRendererAngularComp} from 'ag-grid-angular/main';
+import {IHeaderAngularComp, ICellRendererAngularComp} from 'ag-grid-angular';
 
 
 @Component({
