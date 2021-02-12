5c1313f0987c4bf87c894122227f82f3ef5901e5
Luke Stanley <luke.stanley@gmail.com>
2021-02-12 15:33:33 +0000

    Redirects from any flow URL and makes login button more of a button

--- src/app/blocks/web-money/web-money.component.ts
+++ src/app/blocks/web-money/web-money.component.ts
@@ -45,52 +45,90 @@ function addCoilClientScript() {
   const s = document.createElement('script');
   s.setAttribute('src', 'https://cdn.coil.com/coil-oauth-wm.v7.beta.js');
   document.head.appendChild(s);
+  document['monetization'] = document.createElement('div');
+  document['monetization'].state = 'stopped';
 }
 
 async function setupCoilClient() {
-  let callbackCode = new URLSearchParams(location.search).get('code');
 
-  if (callbackCode) {
-    addCoilClientScript()
-    document['monetization'] = document.createElement('div')
-    document['monetization'].state = 'stopped'
-    let response = await fetch('http://localhost:3000/api/login?code=' + callbackCode, {
+  // Upon Coil Oauth confirmation, we recieve a redirect 
+  // callback from Coil, at a URL like:
+  // https://app.kendra.io/coil/callback?code=something&state=something
+  // we we look for callbacks to this whenever this component
+  // exists to redirect the app to the desired URL
+
+  let codeURLParameter = new URLSearchParams(location.search).get('code');
+  let coilAuthStateParameter = new URLSearchParams(location.search).get('state');
+  let coilOauthCallbackCode = new URLSearchParams(location.search).get(
+    'coilOauthCallbackCode' // long name to ensure exact match
+  );
+
+
+  if (location.pathname === '/coil/callback') {
+    // Callback redirect from Coil found, redirecting to correct app URL
+    // and also passing the coilOauthCallbackCode
+    let invokerURL = atob(coilAuthStateParameter.split('_')[1]);
+    let baseURL = location.protocol + '//' + location.host;
+    location.href = baseURL + invokerURL + '?coilOauthCallbackCode=' + codeURLParameter;
+    return
+  }
+
+  if (coilOauthCallbackCode) {
+    // coilOauthCallbackCode found, so we load the Coil polyfill script,
+    // that implements the Web Monetization API and request the
+    // BTP token for the user, and pass it to the polyfill
+
+    addCoilClientScript();
+
+    let response = await fetch('https://coil-api-proxy.vercel.app/api/login?code=' + coilOauthCallbackCode, {
       method: 'POST'
     });
 
     let result = await response.json();
-    console.dir({ btpToken: result.btpToken })
-    window.history.replaceState(null, document.title, location.href.replace(location.search, ''));
-    document['coilMonetizationPolyfill'].init({ btpToken: result.btpToken })
-  } else { console.log('login?') }
+    if (result.btpToken) {
+      console.info('logged in Coil, and got BTP token');
+
+      // Removes ugly parameters from browser URL:
+      window.history.replaceState(null, document.title, location.href.replace(location.search, ''));
+
+      // Provides BTP token to the polyfill:
+      document['coilMonetizationPolyfill'].init({ btpToken: result.btpToken });
+
+    } else {
+      console.error('Failed to connect with Coil', result);
+      alert('Failed to connect with Coil :(');
+    }
+
+
+  }
 }
 
 
+const oauthState = uuid4() + '_' + btoa(location.pathname);
+// A random UUID with our current path, to redirect back to
 
+const urlPrefix = "https://app.kendra.io/";;
 
+const coilLoginParameters = {
+  response_type:'code',
+  scope:'simple_wm openid',
+  client_id: COIL_CLIENT_ID,
+  state: oauthState,
+  redirect_uri: urlPrefix + 'coil/callback'
+};
 
-function unixTime() {
-  return Math.floor(Date.now() / 1000);
-}
+const coilOauthLoginURL = new URL("https://coil.com/oauth/auth");
+Object.entries(coilLoginParameters).forEach(([key,value])=>{
+  coilOauthLoginURL.searchParams.append(key,value);
+})
 
-const sessionUUID = uuid4();
-const loginURL = `https://coil.com/oauth/auth?response_type=code&scope=simple_wm openid&client_id=` + COIL_CLIENT_ID +
-  `&state=` + sessionUUID + `&redirect_uri=https://app.kendra.io/coil/callback`;
-
-
-const supportCheck = isMonetizationSupported();
-if (supportCheck == false) {
+const webMonetizationSupportFound = isMonetizationSupported();
+if (!webMonetizationSupportFound) {
   setupCoilClient()
-
 }
 const defaultPaymentActiveMessage = `💸▶️ Streaming web payment`;
 const defaultPaymentPausedMessage = '💸⏸️';
 const defaultSupportFoundMessage = '';
-const protectedLoginButtonMessage = `<a href="` + loginURL + `">
-<img src="https://coil.com/images/coil-logo.svg" title="Coil logo"/>
-<br>
-Login with Coil here
-</a>`;
 const defaultSupportMissingMessage = `
 No Web Monetization extension found 😟.
 
@@ -100,8 +138,6 @@ No Web Monetization extension found 😟.
 <br>
 <br>
 <a href="https://coil.com">
-  <img src="https://coil.com/images/coil-logo.svg" title="Coil logo"/>
-  <br>
   Join Coil here.
 </a>`;
 
@@ -118,7 +154,7 @@ export class WebMoneyComponent extends BaseBlockComponent {
   enabled = true;
   supported = isMonetizationSupported();
   showPaymentPointer = true;
-  protectedLoginButtonMessage = protectedLoginButtonMessage;
+  coilLoginURL = coilOauthLoginURL.href;
   supportFoundMessage = defaultSupportFoundMessage;
   supportMissingMessage = defaultSupportMissingMessage;
   paymentActiveMessage = defaultPaymentActiveMessage;
