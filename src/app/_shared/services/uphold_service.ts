62be9efecf1c9f778dbe61d43dfccad812753b54
Luke Stanley <luke.stanley@gmail.com>
2021-09-23 16:36:15 +0100

    Uses JSDoc format for uphold_service.ts

--- src/app/_shared/services/uphold_service.ts
+++ src/app/_shared/services/uphold_service.ts
@@ -10,13 +10,15 @@ const UPHOLD_SITE_ROOT = environment.uphold.siteRoot;
 const UPHOLD_API_ROOT = environment.uphold.proxy;
 const MY_UPHOLD_CARDS = environment.uphold.proxy + 'v0/me/cards/';
 
-export function hasActiveToken() {
-  // To ensure user has a recently authorised token, this check
-  // returns true if we have an Uphold auth token younger than 25 minutes 
-  // and otherwise returns false
+/**
+ * To ensure user has a recently authorised token, this check
+ * returns true if we have an Uphold auth token younger than 25 minutes 
+ * and otherwise returns false
+ */
+export function hasActiveToken(): boolean {
+  const max_time = 25 * 60; // 25 minutes in seconds
   // this is an arbitrary time, as the token expiry date is undocumented:
   // https://uphold.com/en/developer/api/documentation/
-  const max_time = 25 * 60; // 25 minutes in seconds
   const timestamp = localStorage.getItem('uphold_token_timestamp');
   // if no timestamp exists, no active token exists either:
   if (!timestamp) { return false; }
@@ -27,20 +29,25 @@ export function hasActiveToken() {
   return is_fresh;
 }
 
-export function getUpholdOauthLoginURL() {
-  // Returns the URL to redirect to for Uphold OAuth login button.
-  // the URL is constructed like this, to ensure that arbitrary
-  // data from the pathname is encoded safely for use in the
-  // Oauth redirect URL
-  const existing_path = btoa(location.pathname); // encodes path as base64
-  // to uniquely represent this user auth attempt, and the users place here
-  // in our app, we generate a random UUID with the current path appended 
+/**
+ * Returns the URL to redirect to for Uphold OAuth login button.
+ */
+export function getUpholdOauthLoginURL(): string {
+  // The URL is encoded and constructed like this, to ensure 
+  // that arbitrary data from the pathname is encoded safely 
+  // for use in the Oauth redirect URL
+  
+  // Encodes path as base64, to uniquely represent this
+  // user auth attempt, and the users place here in our app,
+  // we generate a random UUID with the current path appended 
   // to allow redirecting the user back to where they were once authorised:
-  const oauthState = uuid.v4() + '_' + existing_path;
+  const oauthState = uuid.v4() + '_' + btoa(location.pathname);
+  
   // A random UUID with our current path, to redirect back to.
   // URL is constructed like this, to ensure that potential 
   // arbitrary user data like the pathname is safely encoded
   const loginURL = new URL(UPHOLD_SITE_ROOT + 'authorize/' + environment.uphold.clientID);
+  
   // now we safely combine the above base URL with the query params
   // to create the full URL for the login button:
   Object.entries({
@@ -52,9 +59,11 @@ export function getUpholdOauthLoginURL() {
   return loginURL.href;
 }
 
-export async function finishAuth() {
-  // Called when the user has successfully logged in to Uphold
-  // and we have received a code from the OAuth server. 
+/**
+ * Called when the user has successfully logged in to Uphold
+ * and we have received a code from the OAuth server. 
+ */
+export async function finishAuth(): Promise<void> {
   // By this point the user has been redirect back to us, and we can now 
   // exchange this code for an access token then direct the user back to 
   // the original page they were on before.
@@ -88,13 +97,22 @@ export async function finishAuth() {
   }
 }
 
-
+/**
+ * UpholdService
+ */
 export class UpholdService {
-  xrp_address_balances = {}; // Uphold card IDs with XRP wallet balance and ILP addresse if exist
-  token = '';
-  balance_address = '';
-  output = undefined;
+  // Uphold card IDs with XRP wallet balance and optional ILP address if exists:
+  xrp_address_balances: { [key: string]: { balance: number, ILP_URL?: string } } = {};
+  // Uphold token:
+  token: string;
+  // Optional Interledger address to get balance for:
+  balance_address: string;
+  // Output to emit the balance to:
+  output: any;
 
+  /**
+   * Constructor
+   */
   constructor(options: any) {
     if (options) {
       this.balance_address = options.balance_address;
@@ -102,16 +120,22 @@ export class UpholdService {
     }
   }
 
+  /**
+   * Get the Uphold token
+   */
   get_token() {
     // Get the Uphold token
     this.token = localStorage.getItem('uphold_token');
     return this.token;
   }
 
-  public async get_xrp_cards_list(only_balance) {
-    // Emits an array of the users XRP cards / wallets, as well as the balance for each.
-    // If only_balance is true, then we only emit the balance for the specified address.
-    // Otherwise we emit the entire list of balances.
+  /**
+   * Emits an array of the users XRP cards / wallets, as well as the balance for each.
+   *
+   * @param only_balance  If only_balance is true, then we only emit the balance for the specified address.
+   * Otherwise we emit the entire list of balances.
+   */
+  public async get_xrp_cards_list(only_balance: boolean) {
 
     const token = this.get_token();
 
@@ -121,10 +145,12 @@ export class UpholdService {
     // and emit our result to 'output'
 
     // TODO: use return instead of emit, to improve service seperation
-
+    
+    /**
+     * Helper function to do authorised calls to the Uphold API
+     * using the users Uphold token and return the data
+     */
     async function oauth_get(url: string) {
-      // This helper function helps do authorised calls to the Uphold API
-      // using the users Uphold token and return the data
       const request = await axios.request({
         method: 'GET',
         url: UPHOLD_API_ROOT + url,
@@ -132,7 +158,6 @@ export class UpholdService {
           'Authorization': 'Bearer ' + parent_this.token
         }
       });
-
       return await request.data;
     }
 
@@ -147,7 +172,7 @@ export class UpholdService {
     await Promise.all(xrp_cards.map(async (card) => {
       // we get the balance for this card:
       const address_data = await oauth_get('v0/me/cards/' + card.id + '/addresses');
-      // we extract the Interledger Protocol address from the response using JMESPath
+      // we extract the Interledger address from the response using JMESPath
       // assuming it exists:
       const ILP_address = search(address_data, `[?type=='interledger'].formats[0].value|[0]`);
       // we get the card from the list of results, to get the balance:
@@ -169,14 +194,16 @@ export class UpholdService {
 
   }
 
+  /**
+   * Makes a new XRP card with Interledger address.
+   * 1. Post a request to make a new XRP card, 
+   * 2. Requests the Interledger address for the card
+   * 3. Emit new card data
+   */
   public async make_card() {
-    // Makes a new XRP card with Interledger address.
-    // 1. Post a request to make a new XRP card, 
-    // 2. Requests the Interledger address for the card
-    // 3. Emit new card data
-
     const token = this.get_token();
     console.assert(token);
+    
     // we emit using the 'output' of this scope,
     // and add the new card to 'xrp_address_balances'
     const parent_this = this;
@@ -214,7 +241,5 @@ export class UpholdService {
 
     // Output the cards, with the new card for flows to make use of:
     parent_this.output.emit(clone(parent_this.xrp_address_balances));
-    // TODO: use return instead of emit, to improve service seperation
   }
-
 }
