e0de9b44838e206fdd1b86a9b921aaba7f0eca41
Greenman <greenman@the-organization.com>
2021-10-15 16:06:22 +0100

    Updated docs. Fixed mapping error catch.

--- src/app/blocks/mapping-block/mapping-block.component.ts
+++ src/app/blocks/mapping-block/mapping-block.component.ts
@@ -35,7 +35,7 @@ export class MappingBlockComponent implements OnInit, OnChanges {
   ngOnChanges(changes): void {
     const keyChanges = Object.keys(changes);
     if (keyChanges.length === 1 && keyChanges.includes('context')) {
-      // ignore changes where only the context changed
+      // ignore changes where only the context changed      
       return;
     }
     // console.log('mapping changes', this.mapping,  changes);
@@ -53,12 +53,16 @@ export class MappingBlockComponent implements OnInit, OnChanges {
       return;
     }
     this.hasError = false;
-    try {
-      
+    try {      
       // console.log(this.mapping, { mappingResult });
       setTimeout(function(){
+        try {
         const mappingResult = this.getMappingResult(this.mapping);
         this.output.emit(clone(mappingResult));
+        } catch (e) {
+          this.hasError = true;
+          this.errorMessage = `${e.message}  \nMapping: ${this.mapping}`;    
+        }
         this.hasRun = true;
       }.bind(this), 0);
     } catch (e) {
