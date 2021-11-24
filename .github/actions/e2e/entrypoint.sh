a8cd07351a14b9bb62910bd05f1984d2f7ef0384
Luke Stanley <luke.stanley@gmail.com>
2021-11-24 17:07:45 +0000

    Fixes wrong bash path

--- .github/actions/e2e/entrypoint.sh
+++ .github/actions/e2e/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh -l
+#!/bin/sh -l
 
 DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 echo "Waiting for url to load:$DEPLOYMENT_URL"
