06fccadef224e4bfadf7f8aa5088331f25f2e20b
Luke Stanley <luke.stanley@gmail.com>
2021-11-24 16:16:10 +0000

    Uses latest NPM, wait for deployment, and logs URL in CI action

--- .github/actions/e2e/entrypoint.sh
+++ .github/actions/e2e/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
 DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
+echo $https://$DEPLOYMENT_URL
+npm install npm@latest
 npm install
+npx wait-on https://$DEPLOYMENT_URL
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
