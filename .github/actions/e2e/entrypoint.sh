c85d4c42c9b7a6f558f243b898efc2e01bad6ba4
Luke Stanley <luke.stanley@gmail.com>
2021-11-24 16:54:47 +0000

    Changes logging to include explanation with URL

--- .github/actions/e2e/entrypoint.sh
+++ .github/actions/e2e/entrypoint.sh
@@ -1,10 +1,8 @@
-#!/bin/sh -l
+#!/usr/bin/sh -l
 
 DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
-echo $DEPLOYMENT_URL
-npm install npm@latest
+echo "Waiting for url to load:$DEPLOYMENT_URL"
 npm install
-echo "Waiting for url to load"
 npx wait-on https-get://$DEPLOYMENT_URL
 echo "URL loaded, running cypress"
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
