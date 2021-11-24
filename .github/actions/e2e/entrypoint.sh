d1d0e367390d116f92aababbc5567169df84299b
Luke Stanley <luke.stanley@gmail.com>
2021-11-24 17:29:09 +0000

    Waits 60 seconds to avoid wrong Vercel domain (redirect?)

--- .github/actions/e2e/entrypoint.sh
+++ .github/actions/e2e/entrypoint.sh
@@ -3,6 +3,6 @@
 DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 echo "Waiting for url to load:$DEPLOYMENT_URL"
 npm install
-npx wait-on https-get://$DEPLOYMENT_URL
+npx wait-on https-get://$DEPLOYMENT_URL -d 60000
 echo "URL loaded, running cypress"
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
