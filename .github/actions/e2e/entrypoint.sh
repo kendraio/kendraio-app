3fd4188039e013b2339690b1e8ced0ad880d9bb4
Luke Stanley <luke.stanley@gmail.com>
2021-11-24 16:36:06 +0000

    Adds better logging to CI action, makes wait-on use HTTPS get test

--- .github/actions/e2e/entrypoint.sh
+++ .github/actions/e2e/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -l
 
 DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
-echo $https://$DEPLOYMENT_URL
+echo $DEPLOYMENT_URL
 npm install npm@latest
 npm install
-npx wait-on https://$DEPLOYMENT_URL
+echo "Waiting for url to load"
+npx wait-on https-get://$DEPLOYMENT_URL
+echo "URL loaded, running cypress"
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
