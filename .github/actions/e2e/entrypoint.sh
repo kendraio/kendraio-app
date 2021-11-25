9a75432bd05a2b9c50dda4d4530ecafc864eb0da
Luke Stanley <luke.stanley@gmail.com>
2021-11-25 13:15:43 +0000

    Install npm modules while waiting

--- .github/actions/e2e/entrypoint.sh
+++ .github/actions/e2e/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
+npm install
 DEPLOYMENT_URL=$(curl --silent --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 echo "Waiting for url to load:$DEPLOYMENT_URL"
 STATE=$(curl --silent --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].state')
@@ -12,6 +13,6 @@ do
 done
 
 echo "The deployment at:$DEPLOYMENT_URL should be complete"
-npm install
+
 echo "URL loaded, running cypress"
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
