190706f18ae4d6c1da6308c7305ba1a62eb22a7c
Luke Stanley <luke.stanley@gmail.com>
2021-11-25 13:01:41 +0000

    Wait better

--- .github/actions/e2e/entrypoint.sh
+++ .github/actions/e2e/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -l
-
 DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 echo "Waiting for url to load:$DEPLOYMENT_URL"
+STATE=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].state')
+
+while [ "$STATE" != "READY" ]
+do
+  echo "waiting"    
+  sleep 5
+  STATE=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].state')
+done
+
+echo "The deployment at:$DEPLOYMENT_URL should be complete"
 npm install
-npx wait-on https-get://$DEPLOYMENT_URL -d 60000
 echo "URL loaded, running cypress"
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
