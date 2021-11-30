520d6a4e00c981b88f2d88eee575a6ab16c007cc
Luke Stanley <luke.stanley@gmail.com>
2021-11-30 16:59:48 +0000

    Adds Karma unit tests to GitHub Action start

--- .github/actions/e2e/entrypoint.sh
+++ .github/actions/e2e/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 npm install
+echo "Starting Karma unit tests"
+npx ng test -- --watch=false
+
 DEPLOYMENT_URL=$(curl --silent --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 echo "Waiting for url to load:$DEPLOYMENT_URL"
 STATE=$(curl --silent --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].state')
