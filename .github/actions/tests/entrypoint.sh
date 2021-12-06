a31ddd2ca727ef8c7916811838244569debf914c
Luke Stanley <luke.stanley@gmail.com>
2021-12-06 14:45:12 +0000

    Renames action to tests and passes key as 3rd input

--- .github/actions/tests/entrypoint.sh
+++ .github/actions/tests/entrypoint.sh
@@ -3,6 +3,7 @@ echo "::group::General setup"
 echo '::echo::off'
 # Assert the key exists, and exit with code 404 if not
 echo '::echo::on'
+export CYPRESS_RECORD_KEY=$3
 echo -n "$CYPRESS_RECORD_KEY" | sha1sum | awk '{print $1}'
 if [ ! -f "$CYPRESS_RECORD_KEY" ]; then
   echo "::error:: Could not find cypress record key"
