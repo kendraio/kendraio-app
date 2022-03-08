cb0027a74a0befb85aebe10a81e8784d8c0b9279
Greenman <greenman@the-organization.com>
2022-03-08 17:13:41 +0000

    Save workflow to local db

--- src/app/dialogs/save-workflow-dialog/save-workflow-dialog.component.ts
+++ src/app/dialogs/save-workflow-dialog/save-workflow-dialog.component.ts
@@ -1,9 +1,10 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {has, get} from 'lodash-es';
+import {has, get, findIndex} from 'lodash-es';
 import {environment} from '../../../environments/environment';
-
+import {LocalDatabaseService} from '../../services/local-database.service';
+import {WorkflowRepoService} from '../../services/workflow-repo.service';
 @Component({
   selector: 'app-save-workflow-dialog',
   templateUrl: './save-workflow-dialog.component.html',
@@ -20,6 +21,7 @@ export class SaveWorkflowDialogComponent implements OnInit {
   }
 
   constructor(
+    private localData: LocalDatabaseService,
     public dialogRef: MatDialogRef<SaveWorkflowDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data,
     private readonly http: HttpClient
@@ -75,7 +77,44 @@ export class SaveWorkflowDialogComponent implements OnInit {
     });
   }
 
+  saveToAdapter() {
+    const { id, adapterName } = this.data;
+        
+    this.localData['workflows'].where('[adapterName+workflowId]').equals([adapterName || 'UNKNOWN', id]).count().then(
+      (count) => {
+    
+      if (count === 0) {
+        this.localData['workflows'].add({ adapterName: adapterName, id: id, blocks: this.data.blocks, title: this.data.title, workflowId: id, modified: true });      
+        this.dialogRef.close();
+      } else {
+      this.localData['workflows']
+        .where('[adapterName+workflowId]')
+        .equals([adapterName || 'UNKNOWN', id])
+        .modify({ blocks: this.data.blocks, title: this.data.title, workflowId: id, modified: true })
+        .then(() => {
+          this.localData['adapters'].get(adapterName).then(adapter => {
+            const workflow = get(adapter, 'workflow', []);
+            const workflowIndex = findIndex(workflow, ({ workflowId }) => workflowId === id);
+            if (workflowIndex !== -1) {
+              workflow[workflowIndex] = { ...workflow[workflowIndex], title: this.data.title, modified: true };
+            }
+            this.localData['adapters'].update(adapterName, { ...adapter, workflow, modified: true }).then(() => {
+              this.dialogRef.close();
+            });
+          });
+        })
+        .catch(error => {
+          console.log({ error })        
+        });
+      }
+      }
+    );
+  }
+  
+
   onCancel() {
     this.dialogRef.close();
   }
+
+ 
 }
