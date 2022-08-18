297d6191cd9f95202bfd77f3c38fd2073c2c7187
Luke Stanley <luke.stanley@gmail.com>
2022-08-18 14:19:26 +0100

    Adds update test case, renames insert case

--- cypress/integration/db_block_metadata_editing.ts
+++ cypress/integration/db_block_metadata_editing.ts
@@ -1,5 +1,6 @@
 import { loadFlowCode } from '../support/helper';
-import dexie from 'dexie';
+import { clearMetadataTable } from '../support/helper';
+import { getMetadataTable } from '../support/helper';
 
 // tslint:disable: quotemark
 /// <reference types="Cypress" />
@@ -19,25 +20,22 @@ describe('Kendraio context and state', () => {
       statusCode: 200,
       response: {}
     }).as('fonts'); // empty response
+
+  });
+
+  afterEach(() => {
+    clearMetadataTable();
   });
 
 
-  describe('Ensure unpsert can insert and update records in the metadata table', () => {
-    it('should be able to insert a record into a blank table', async () => {
-      let db = new dexie('kendraio-db');
-      // ensure database is blank:
-      await db.delete();
-
+  describe('Ensure upsert can insert and update records in the metadata table', () => {
+    it('should be able to insert a new record', () => {
       // TODO: if we use init the flow runs twice, is this expected? Will Dixie raise any errors? 
       // I might have seen one, I suspected the flow was interrupted before it can finish, this needs investigation.
       loadFlowCode([
         //{ 
         //  "type": "init"
         //},
-        {
-          "type": "template",
-          "template": "Loaded."
-        },
         {
           "type": "mapping",
           "mapping": "{ uuid: uuid('test'), name: 'test' }"
@@ -58,24 +56,106 @@ describe('Kendraio context and state', () => {
         },
         {
           "type": "debug",
-          "open": 1,
+          "open": 3,
           "showData": true,
           "showContext": false,
           "showState": false
+        }, {
+          "type": "template",
+          "template": "Loaded."
         }
       ]);
-      cy.contains('Loaded.').wait(1000).then(async () => {
+
+      cy.window().should('have.property', 'databaseInit', true);
+
+      cy.get('mat-spinner').should('not.exist');
+      cy.get('body').contains('Loaded.');
+      cy.wrap(getMetadataTable()).then((metadata) => {
         // check that 1 item exists in the metadata table
-        db = new dexie('kendraio-db');
-        await db.open();
-        // assert metadata table has one record:
-        const metadata = await db.table('metadata').toArray();
         expect(metadata.length).to.equal(1);
         // assert that "test" is in the metadata table:
         expect(metadata[0].data.name).to.equal('test');
       });
 
     });
+
+    it('should be able to update an existing record', () => {
+      // TODO: if we use init the flow runs twice, is this expected? Will Dixie raise any errors? 
+      // I might have seen one, I suspected the flow was interrupted before it can finish, this needs investigation.
+      loadFlowCode([
+        {
+          "type": "mapping",
+          "mapping": "{ uuid: uuid('test'), name: 'test' }"
+        },
+        {
+          "type": "db",
+          "operation": "upsert",
+          "adapterName": "UNKNOWN",
+          "schema": "none",
+          "blockComment": ""
+        },
+        {
+          "type": "db",
+          "operation": "get",
+          "adapterName": "UNKNOWN",
+          "schema": "none",
+          "blockComment": ""
+        },
+        {
+          "type": "debug",
+          "open": 3,
+          "showData": true,
+          "showContext": false,
+          "showState": false
+        }, 
+        {
+          "type": "mapping",
+          "mapping": "{ uuid: uuid('test'), name: 'test2'}"
+        },
+        {
+          "type": "db",
+          "operation": "upsert",
+          "adapterName": "UNKNOWN",
+          "schema": "none",
+          "blockComment": ""
+        },
+        {
+          "type": "db",
+          "operation": "get",
+          "adapterName": "UNKNOWN",
+          "schema": "none",
+          "blockComment": ""
+        },
+        {
+          "type": "debug",
+          "open": 3,
+          "showData": true,
+          "showContext": false,
+          "showState": false
+        },
+        {
+          "type": "template",
+          "template": "Loaded."
+        }
+      ]);
+
+      cy.window().should('have.property', 'databaseInit', true);
+
+
+      cy.get('mat-spinner').should('not.exist');
+      cy.get('body').contains('Loaded.');
+      cy.contains('name:"test"');
+      cy.contains('name:"test2"');
+      cy.wrap(getMetadataTable()).then((metadata) => {
+        // check that 1 item exists in the metadata table
+        expect(metadata.length).to.equal(1);
+        // assert that "test2" is in the metadata table:
+        expect(metadata[0].data.name).to.equal('test2');
+      });
+
+
+    });
+
   });
 
 
