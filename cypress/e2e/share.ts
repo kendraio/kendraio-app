10b76c2222f08caa0bf034317288a144d0a8a2eb
Luke Stanley <luke.stanley@gmail.com>
2022-10-20 18:53:39 +0100

    Test sharing encoding of database

--- cypress/e2e/share.ts
+++ cypress/e2e/share.ts
@@ -20,13 +20,26 @@ describe('Share functionality', () => {
 
     });
 
-
     it('should assert a shared database loads as expected using given URL', () => {
-        cy.visit("/workflow-builder?metadata=NobwRArhCWAmYC4wBYAMBWdAzVB2AzALS4CMApgJyHIAcJJhNuATAMaFmr4HquoUA2SmAA0YAIaxxABwAuZAE4A5cQFsyiMAGdWACzKrxW0dr0HxK9ZukLo6gPqroAO2hb5C42KmzxicM5qGkgAQgD2tsYAvmIANuIARmSxmuGRYDHgUHCapMwJ6JTsyLhkuNQU4uiMNMgChFwU+KhksLjIdfBikjIelsGm+oZeg+b91rYOTq7uiiM+fggBQZoAMtAAXhlxiclrm9tZMPBIZeLM9PiwhEll1Kiw1xSY1eKCAshYNPhYJAl+3SkckU4yQOiGRhM4LGKzBZmGJgW-kgx00ZwuJCuNzIdzQj0Iz0whDeAg+Xx+fwBYBsYWkilk0DIxgQoDAAGsyABPTSBKxiBmyWIDcb8zl0zQAFTIAA9ZBkALpiXkDGx2MiOFxuDzzJmsWxyaBhZyaAAKAFkAASxLUmeJJFJIZXbMB2vZIVVTTWzTwKoA")
-        .contains("Workflow");
+        const databaseUrl = "/workflow-builder?metadata=NobwRArhCWAmYC4wBYAMBWdAzVB2AzALS4CMApgJyHIAcJJhNuATAMaFmr4HquoUA2SmAA0YAIaxxABwAuZAE4A5cQFsyiMAGdWACzKrxW0dr0HxK9ZukLo6gPqroAO2hb5C42KmzxicM5qGkgAQgD2tsYAvmIANuIARmSxmuGRYDHgUHCapMwJ6JTsyLhkuNQU4uiMNMgChFwU+KhksLjIdfBikjIelsGm+oZeg+b91rYOTq7uiiM+fggBQZoAMtAAXhlxiclrm9tZMPBIZeLM9PiwhEll1Kiw1xSY1eKCAshYNPhYJAl+3SkckU4yQOiGRhM4LGKzBZmGJgW-kgx00ZwuJCuNzIdzQj0Iz0whDeAg+Xx+fwBYBsYWkilk0DIxgQoDAAGsyABPTSBKxiBmyWIDcb8zl0zQAFTIAA9ZBkALpiXkDGx2MiOFxuDzzJmsWxyaBhZyaAAKAFkAASxLUmeJJFJIZXbMB2vZIVVTTWzTwKoA";
+        const flowUrl = "/workflow-builder?data=NobwRALgngDgpmAXGA5gewM4FcBGYA0YAhgCZEwRwBOAckQLYLIYDGAFnPURgWAO5oqAawBmAGzR8AkiSRhWHLj0I4JLIQGE09RgDsIcgDJpSAAgWduYAL75w0eHICWupwdv3YTSJxhiilLyU9H4B3gA8bABMAHwAIgFEONxwpixo nD6GOEA9NExNnaQXnJcMDAuKLzllbrVyGQQRMAA-FhYTiQAvN0ABnAA7ERRAIyjAMwkALQ4cEPTACwADCQzAJwArNvTROsAbPuLIgAcEyKjyX0AuqYAPqbAy9e8qmjqWjpZBsgAYk5iShUczsSymCBoUxYKhiUwwIhUBhFTyOZDpTIADwg0wwRAAbghCOjKFiANJwKByCxcV5qTTaPQ-eT41JoaHmeAsJwiJwsEGKIjIkqosCMDC4lCEyBuMTeQxODAQUxuTgYUwiKjafmWRCmEAgNIZEkQAB01KIJt0DFS1msQoc3hIeEIaHgiIgTgycklBkIpHIQLojCpoKUvHNAHE4BAgXJiXAsWbQxarcGVHTPoyjCYSKYmkR1Zr6NqaR5hd5alUauQ6g0wPngNcTYwqJKABQAAXwetM-jmYl1Ps73YAJCb44nzSa 3AxABKaxz2nvelffRyKNKiAcUwAHWlEFl 9M9tKjTgOCw1Rd8F0SFGhAwbEkCWaSAgVCwcEfz74WkxPwiEQYgYN 8i-gAys0gSIEBIFwGWDrelQXS8CgKEkAA8hQnq6DwiDgPCKAuAEuHvp YFESRHoZAACkQkoQU4ABeTArLYYDpGIWD0LocRwCI GgGAHCkNQQZykks68Dys6yMgM5iEKokkOJ1pyNh1CkRkyicbOYgAEpZKpVDUHIAjCOIkhGboJlmUS k2XZVD0Yi9D4eAbzqEJKLeEQLA0XhrxYDGOlIMJilyAAoiQbjLt54W XI-hYLo7C8P6FBmcwya6RZogSNI8lgHAsUQFIwS8BOPzgOaMhxkaCamgAjl VBQK5DAYEmAr1YQqGNIkJodKhtrXIQWRJLK8kfl 1jXPNtrFCpanBsgUhxDJThyXII2yPN6Yrpm3xyKeIr YFumXqFQWIBFUlKcgACCaymBVnBVWgEhUHIqlAVggLxUIPnlslRCpelfpkFlP05QKeWCAVkj1cgpVuO99CfQBSC1cmKOcY1iatdQHUIl1PWWH1YADWAe1tkuY0TVaqileRc3FJFyDyoqpjAbC5pXXSINIQp4NpWwGXQ7GcOWAjlmFfjYgKu440lcz01swhqteauWbIGd3joNgzrEFLq3eE4ISCLGhD5VZRVyCIaBoDbYA68d65-M7QK7ne81AA";
+
+        cy.visit(databaseUrl)
+            .contains("Workflow");
+
         cy.wait(1000);
-        cy.visit("/workflow-builder?data=NobwRALgngDgpmAXGA5gewM4FcBGYA0YAhgCZEwRwBOAckQLYLIYDGAFnPURgWAO5oqAawBmAGzR8AkiSRhWHLj0I4JLIQGE09RgDsIcgDJpSAAgWduYAL75w0eHICWupwdv3YTSJxhiilLyU9H4B3gA8bABMAHwAIgFEONxwpixo nD6GOEA9NExNnaQXnJcMDAuKLzllbrVyGQQRMAA-FhYTiQAvN0ABnAA7ERRAIyjAMwkALQ4cEPTACwADCQzAJwArNvTROsAbPuLIgAcEyKjyX0AuqYAPqbAy9e8qmjqWjpZBsgAYk5iShUczsSymCBoUxYKhiUwwIhUBhFTyOZDpTIADwg0wwRAAbghCOjKFiANJwKByCxcV5qTTaPQ-eT41JoaHmeAsJwiJwsEGKIjIkqosCMDC4lCEyBuMTeQxODAQUxuTgYUwiKjafmWRCmEAgNIZEkQAB01KIJt0DFS1msQoc3hIeEIaHgiIgTgycklBkIpHIQLojCpoKUvHNAHE4BAgXJiXAsWbQxarcGVHTPoyjCYSKYmkR1Zr6NqaR5hd5alUauQ6g0wPngNcTYwqJKABQAAXwetM-jmYl1Ps73YAJCb44nzSa 3AxABKaxz2nvelffRyKNKiAcUwAHWlEFl 9M9tKjTgOCw1Rd8F0SFGhAwbEkCWaSAgVCwcEfz74WkxPwiEQYgYN 8i-gAys0gSIEBIFwGWDrelQXS8CgKEkAA8hQnq6DwiDgPCKAuAEuHvp YFESRHoZAACkQkoQU4ABeTArLYYDpGIWD0LocRwCI GgGAHCkNQQZykks68Dys6yMgM5iEKokkOJ1pyNh1CkRkyicbOYgAEpZKpVDUHIAjCOIkhGboJlmUS k2XZVD0Yi9D4eAbzqEJKLeEQLA0XhrxYDGOlIMJilyAAoiQbjLt54W XI-hYLo7C8P6FBmcwya6RZogSNI8lgHAsUQFIwS8BOPzgOaMhxkaCamgAjl VBQK5DAYEmAr1YQqGNIkJodKhtrXIQWRJLK8kfl 1jXPNtrFCpanBsgUhxDJThyXII2yPN6Yrpm3xyKeIr YFumXqFQWIBFUlKcgACCaymBVnBVWgEhUHIqlAVggLxUIPnlslRCpelfpkFlP05QKeWCAVkj1cgpVuO99CfQBSC1cmKOcY1iatdQHUIl1PWWH1YADWAe1tkuY0TVaqileRc3FJFyDyoqpjAbC5pXXSINIQp4NpWwGXQ7GcOWAjlmFfjYgKu440lcz01swhqteauWbIGd3joNgzrEFLq3eE4ISCLGhD5VZRVyCIaBoDbYA68d65-M7QK7ne81AA")
-        .contains('Boris');
+
+        cy.visit(flowUrl)
+            .contains('Boris');
         cy.contains('Liz');
+
+        // Now we press the share button, and select share database:
+        cy.get('mat-icon').contains("settings").click();
+        cy.get('mat-icon').contains("share").click();
+        cy.get('button:contains("Share database")').click();
+
+        // assert shareValue is in the databaseUrl
+        cy.get('app-show-share-link-dialog textarea').should('contain', databaseUrl);
     });
+
 });
