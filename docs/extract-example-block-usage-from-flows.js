fc52dac3efdeb6682ed153e14e3db1efd6db6943
Luke Stanley <luke.stanley@gmail.com>
2023-10-24 14:49:58 +0100

    Adds flow-analysis folder to save flows & analysis

--- docs/extract-example-block-usage-from-flows.js
+++ docs/extract-example-block-usage-from-flows.js
@@ -13,8 +13,8 @@
  */
 
 
-let inputFilePath = 'flows.json';
-let outputFilePath = 'flow-block-config-analysis.json';
+let inputFilePath = 'flow-analysis/flows.json';
+let outputFilePath = 'flow-analysis/flow-block-config-analysis.json';
 let blockType = 'form';  // Replace with the block type you're interested in
 let propertyOfInterest = null;  // E.g: 'uiSchema' - replace with the property you're interested in or leave as null
 
