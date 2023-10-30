c9103dd616f03fe8703ca3f4c132082c46e6a97f
gio <designbygio@pm.me>
2023-10-30 17:08:43 +0100

    update cypress to v13

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,30 +1,35 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  videosFolder: 'cypress/videos',
+  videosFolder: "cypress/videos",
   video: false,
-  screenshotsFolder: 'cypress/screenshots',
-  fixturesFolder: 'cypress/fixtures',
-  reporter: 'mochawesome',
+  screenshotsFolder: "cypress/screenshots",
+  fixturesFolder: "cypress/fixtures",
+  reporter: "mochawesome",
+
   reporterOptions: {
-    reportDir: 'cypress/results',
+    reportDir: "cypress/results",
     overwrite: false,
     html: true,
     json: true,
   },
+
   watchForFileChanges: false,
-  projectId: 'ebzwp1',
+  projectId: "ebzwp1",
   pageLoadTimeout: 100000,
   defaultCommandTimeout: 30000,
+
   retries: {
     runMode: 2,
     openMode: 2,
   },
+
   experimentalStudio: true,
+
   e2e: {
     setupNodeEvents(on, config) {},
     supportFile: false,
-    baseUrl: 'http://localhost:4200',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    baseUrl: "http://localhost:4200",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
-})
+});
