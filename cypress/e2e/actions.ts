712afff755e44d74d2c9f0072978de3797bb9657
gio <designbygio@pm.me>
2024-02-01 11:35:45 +0100

    improved text

--- cypress/e2e/actions.ts
+++ cypress/e2e/actions.ts
@@ -1,26 +1,9 @@
 import { loadFlowCode } from '../support/helper';
 
-// tslint:disable: quotemark
 /// <reference types="Cypress" />
 
 
 describe('Actions block type', () => {
-
-    // beforeEach(() => {
-    //     // Prevent external network request for adapter config
-    //     cy.intercept('GET', 'https://kendraio.github.io/kendraio-adapter/config.json', {
-    //         fixture: 'adapterConfig.json'
-    //     });
-
-    //     // Prevent external network requests for Workflow cloud
-    //     cy.intercept('GET', 'https://app.kendra.io/api/workflowCloud/listWorkflows', {
-    //         fixture: 'workflow-cloud.json'
-    //     });
-
-    //     // Prevent external network requests for fonts with empty CSS rule 
-    //     cy.intercept('https://fonts.googleapis.com/\*\*', "\*{ }");
-    // });
-
     it('should display a button with label, id and default color', () => {
 
         loadFlowCode([
