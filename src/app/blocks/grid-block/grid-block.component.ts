0428741bbf28e08b5a6fe2fe3a5969e79a5b727e
designbygio <gsambrotta@users.noreply.github.com>
2024-02-13 13:39:41 +0100

    Fix Ag Grid UI and custom component render (#447)
    * restore correct UI style to grid
    
    * apply breaking changes with frmeworkComponents, bump version to 31

--- src/app/blocks/grid-block/grid-block.component.ts
+++ src/app/blocks/grid-block/grid-block.component.ts
@@ -34,7 +34,7 @@ export class GridBlockComponent implements OnInit, OnChanges {
   valueGetter = null; 
 
 
-  frameworkComponents = {
+  components = {
     workflowRenderer: WorkflowCellRendererComponent,
     connectionStatusRenderer: ConnectionStatusRendererComponent,
   };
