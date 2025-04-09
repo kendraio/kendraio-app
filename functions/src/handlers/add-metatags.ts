4258480a834ece719c72019a04fe6ffbe92a0a32
Luke Stanley <306671+lukestanley@users.noreply.github.com>
2025-04-09 23:12:21 +0000

    Firebase hosting fix: Serve main JavaScript files directly when not handled by Firebase

--- functions/src/handlers/add-metatags.ts
+++ functions/src/handlers/add-metatags.ts
@@ -2,9 +2,29 @@ const fs = require('fs').promises;
 
 export const addMetatagsHandler = async (req, resp) => {
 
-    if (req.path.endsWith('.js') || req.path.endsWith('.css')) {
-        // Let Firebase hosting try to serve it
-        return resp.status(404).end();
+    if (req.path.endsWith('.js') && req.path.includes('main.')) {    
+        // Firebase hosting should have already handled this but
+        // since it has not, we need to handle it here.
+        // This is a workaround for the Firebase hosting issue
+
+        // We get the exact filename of the file from the request,
+        // since it has a hash.
+        const filename = req.path.split('/').pop();
+        // we read the file from the public directory
+        const filePath = `${process.cwd()}/${filename}`;
+        // we read the file and send it as a response
+        try {
+            const file = await fs.readFile(filePath, 'utf8') as string;
+            // Set the content type to application/javascript
+            resp.setHeader('Content-Type', 'application/javascript');
+            resp.status(200).send(file);
+            return;
+          } catch (error) {
+            console.error(`Error serving ${filename}:`, error);
+            resp.status(404).send('File not found');
+            return;
+        }
+
     }  
     // Otherwise continue with meta tag processing
 
@@ -34,4 +54,5 @@ export const addMetatagsHandler = async (req, resp) => {
 <meta property="og:type" content="website">
 `;
     resp.status(200).send(`${data.replace('</head>', `${metatags}</head>`)}`);
+    return
 };
