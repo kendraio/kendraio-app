06b13c6e1c4e51af9a713d3ce521eb48388b8a2e
Luke Stanley <306671+lukestanley@users.noreply.github.com>
2025-04-10 09:26:04 +0000

    Firebase hosting fixing: Add back dynamic index function with index.html redirect

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,9 +5,9 @@ admin.initializeApp();
 const db = admin.firestore();
 const auth = admin.auth();
 
-//import {addMetatagsHandler} from './handlers/add-metatags';
+import {addMetatagsHandler} from './handlers/add-metatags';
 import appFactory from './handlers/workflow-cloud';
 
 
-//export const dynamicIndex = functions.https.onRequest(addMetatagsHandler);
+export const dynamicIndex = functions.https.onRequest(addMetatagsHandler);
 export const api = functions.https.onRequest(appFactory({ db, auth }));
